8ee443a968aad61c62d3d77b388411c697cb1938
Hrabal <federicocerchiari@gmail.com>
2017-11-18 12:05:20 +0100

    Basic app files.

---
+++ app.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import locale
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import logging
+
+locale.setlocale(locale.LC_TIME, locale.getlocale())
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+db = SQLAlchemy(app)
+
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+import views, models
---
+++ run.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from app import app
+app.run(debug=True)
