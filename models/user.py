502acb6d58ae7fde54207fa3fa4cf768a0e76535
Hrabal <federicocerchiari@gmail.com>
2017-11-18 13:07:46 +0100

    user and login basics

---
+++ models/user.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import bcrypt
+from sqlalchemy.ext.hybrid import hybrid_property
+
+from app import db
+from models.dbtools import Dictable
+
+class User(db.Model, Dictable):
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(64), index=True, unique=True,
+                         nullable=False)
+    password_hash = db.Column(db.String(300))
+    email = db.Column(db.String(300))
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password_hash = bcrypt.hashpw(password.encode('utf-8'),
+                                           bcrypt.gensalt())
+        self.email = email
+
+    def validate_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password_hash)
+
+    @hybrid_property
+    def password(self):
+        return self.password_hash
+
+    @password.setter
+    def password_setter(self, password):
+        self.password_hash = bcrypt.hashpw(password.encode('utf-8'),
+                                           bcrypt.gensalt())
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.user_id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
---
+++ views/user.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from flask import g
+from flask_login import current_user, login_user, logout_user, login_required
+
+from app import app, login_manager
+from models.user import User
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        login_page = LoginPage().set_referrer(request.referrer)
+        if user:
+            form_password = request.form['password'].encode('utf-8')
+            if bcrypt.checkpw(form_password, user.password):
+                login_user(user, remember=True)
+                app.logger.debug('Logged in user %s', user.username)
+                return redirect(request.form['referrer'])
+            else:
+                login_page.set_form(request.form)
+                return login_page.wrong('password').render()
+        else:
+            return login_page.wrong('username').render()
+    return LoginPage().set_referrer(request.referrer).render()
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(request.referrer)
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return Page401().render()
+
+
+@app.route('/edit_user')
+@app.route('/edit_user/<user_id>')
+@login_required
+def user_handler(**kwargs):
+    return UserEdit(data=kwargs).render()
+
+
+@app.route('/api/save_user', methods=['POST', ])
+@login_required
+def api_save_user():
+    user_dict = request.form.to_dict()
+    user = save_user(user_dict)
+    return redirect('/edit_user/%d' % user.id)
+
+
+@app.route('/api/delete_user/<user_id>')
+@login_required
+def api_delete_user(**kwargs):
+    delete_user(kwargs['user_id'])
+    return redirect(request.referrer)
