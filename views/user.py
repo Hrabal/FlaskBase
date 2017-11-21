f7737dc3c8fa18696a9d251b7b02c682e35cb4f6
Federico Cerchiari <federicocerchiari@gmail.com>
2017-11-21 09:10:05 +0100

    un-Tempy-ed login views

--- views/user.py
+++ views/user.py
@@ -5,6 +5,17 @@ from flask_login import current_user, login_user, logout_user, login_required
 from app import app, login_manager
 from models.user import User
 
+TEMP_USER_FORM = """
+<form class="form" action="/login" method="post">
+    {error}
+    <input type="text" name="username" required/>
+        <label for="username">Username</label>
+    <input type="password" name="password" required/>
+        <label for="password">Password</label>
+    <input type="hidden" name="referrer" value="{referrer}"/>
+    <button type="submit">Login</button>
+</form>
+"""
 
 @app.before_request
 def before_request():
@@ -15,44 +26,21 @@ def before_request():
 def login():
     if request.method == 'POST':
         user = User.query.filter_by(username=request.form['username']).first()
-        login_page = LoginPage().set_referrer(request.referrer)
+        login_page = 
         if user:
             form_password = request.form['password'].encode('utf-8')
             if bcrypt.checkpw(form_password, user.password):
                 login_user(user, remember=True)
-                app.logger.debug('Logged in user %s', user.username)
                 return redirect(request.form['referrer'])
             else:
                 login_page.set_form(request.form)
-                return login_page.wrong('password').render()
+                return TEMP_USER_FORM.format(referrer=request.referrer, error='Wrong Password')
         else:
-            return login_page.wrong('username').render()
-    return LoginPage().set_referrer(request.referrer).render()
+            return TEMP_USER_FORM.format(referrer=request.referrer, error='Wrong Username')
+    return TEMP_USER_FORM.format(referrer=request.referrer, error='')
 
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(request.referrer)
-
-
-@app.route('/edit_user')
-@app.route('/edit_user/<user_id>')
-@login_required
-def user_handler(**kwargs):
-    return UserEdit(data=kwargs).render()
-
-
-@app.route('/api/save_user', methods=['POST', ])
-@login_required
-def api_save_user():
-    user_dict = request.form.to_dict()
-    user = save_user(user_dict)
-    return redirect('/edit_user/%d' % user.id)
-
-
-@app.route('/api/delete_user/<user_id>')
-@login_required
-def api_delete_user(**kwargs):
-    delete_user(kwargs['user_id'])
     return redirect(request.referrer)
